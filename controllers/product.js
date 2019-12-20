73705468fc5a2d7a5388a5bfd7510dca1c6dbd95
Leonardo Coutinho Gomes <lcoutinhog@indracampos.local>
2019-12-20 18:00:36 -0300

    Iniciando persistência com filesystem module.

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req,res,next) => {
     //renderização com handlebars
@@ -16,7 +16,8 @@ exports.getAddProduct = (req,res,next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title, price: req.body.price});   //recupera os dados digitados no form pelo body da request
+    const product = new Product(req.body.title, req.body.price);   //recupera os dados digitados no form pelo body da request
+    product.save();
     res.redirect('/'); 
 };
 
@@ -27,9 +28,29 @@ exports.getProducts = (req, res, next) => {
     // passando a lista de produtos com o nome de prods
     //renderização com handlebars
     //res.render('product', { prods: products, pageTitle: 'Produtos', hasProduct: (products.length > 0), activeShop: true }); 
-    
-    //renderização com pug e ejs
-    res.render('product', { prods: products, pageTitle: 'Produtos', hasProduct: (products.length > 0), path: '/' }); 
+
+    //da forma descrita abaixo o código fica sincrono e a pagina é renderizada antes da leitura do arquivo
+
+    /*      //criando um novo array que receberá os produtos
+            //chama o metodo static que recupera todos os produtos
+            const products = Product.fetchAll();
+            
+            //renderização com pug e ejs
+            res.render('product', { prods: products, pageTitle: 'Produtos', path: '/' });  */
+
+    //segue a forma assincrona
+    //chamo o fetchAll que tem como parametro uma função de callback que vai renderizar a pagina
+    //no model o fetchAll vai ler um arquivo e após a leitura chamará
+    // a função de callback passando como parametro um array vazio ou
+    // o array preenchido com os produtos que foram lidos do arquivo
+    Product.fetchAll(products => {
+        res.render('product', 
+                    { 
+                        prods: products,
+                        pageTitle: 'Produtos', 
+                        path: '/' 
+                    });
+        });
 
     // código para pagina estatica
     //  res.sendFile(path.join(__dirname,'../', 'views', 'product.html')); //path.join() concatena o caminho
