e396682151520fcef516212bc8bcea6603510999
Leonardo Coutinho Gomes <lcoutinhog@indracampos.local>
2019-12-28 17:35:28 -0300

    Implementando carrinho de compras

---
+++ models/cart.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const path = require('path');
+
+const p = path.join(__dirname, '..', 'public', 'data', 'cart.json');
+
+module.exports = class Cart {
+    static addProduct(id) {
+        //Fetch the previous cart
+
+    }
+}
